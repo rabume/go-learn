4062f3e27920d68b16afb7455efbb12b75a11c9c
rabume <info@rabu.me>
2022-12-24 16:11:45 +0100

    Lection: Variables

--- app/main.go
+++ app/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi there!")
+
+	// Go will set the datatype int for this variable
+	deckSize := 20
+	fmt.Println(deckSize)
+
+	// var card string = "Ace of spades"
+	// Only need to use the ':' when we define a variable
+	card := "Ace of Spades"
+
+	// When assigning a new value to a already existing variable
+	// you can just use '='
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
