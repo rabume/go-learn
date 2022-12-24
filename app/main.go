5949a226ccb5bc1b19fa21d09fa3f1084dd12bb8
rabume <info@rabu.me>
2022-12-24 16:21:03 +0100

    Lection: Functions and return types

--- app/main.go
+++ app/main.go
@@ -5,18 +5,11 @@ import (
 )
 
 func main() {
-
-	// Go will set the datatype int for this variable
-	deckSize := 20
-	fmt.Println(deckSize)
-
-	// var card string = "Ace of spades"
-	// Only need to use the ':' when we define a variable
-	card := "Ace of Spades"
-
-	// When assigning a new value to a already existing variable
-	// you can just use '='
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
