2f3da79b75675af6f048a664db999874749a03db
rabume <info@rabu.me>
2022-12-25 22:48:27 +0100

    Used interfaces in a example

--- app/main.go
+++ app/main.go
@@ -4,6 +4,31 @@ import (
 	"fmt"
 )
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type germanBot struct{}
+
 func main() {
-	fmt.Println("Hi there!")
+	eb := englishBot{}
+	sb := germanBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (eb englishBot) getGreeting() string {
+	// Custom logic here
+	return "Hello there!"
+}
+
+func (gb germanBot) getGreeting() string {
+	// Custom logic here
+	return "Guten Tag!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
