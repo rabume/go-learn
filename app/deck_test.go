c47b1a3275177d414dd5dd81d41ce97ee76560dc
rabume <info@rabu.me>
2022-12-25 16:51:04 +0100

    Add more tests

--- app/deck_test.go
+++ app/deck_test.go
@@ -6,8 +6,18 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	deckSize := 16
+	firstCard := "Ace of Spades"
+	lastCard := "Four of Clubs"
 
 	if len(d) != deckSize {
 		t.Errorf("Expected deck length of %v, but got %d", deckSize, len(d))
+	}
+
+	if d[0] != firstCard {
+		t.Errorf("Expected %v, but got %v", firstCard, d[0])
+	}
+
+	if d[len(d)-1] != lastCard {
+		t.Errorf("Expected %v, but got %v", lastCard, d[len(d)-1])
 	}
 }
