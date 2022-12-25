1bb1b41756779db7df35aec5f055866ccbcf77b6
rabume <info@rabu.me>
2022-12-25 17:00:41 +0100

    Finished card project lection

--- app/deck_test.go
+++ app/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -20,4 +23,21 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != lastCard {
 		t.Errorf("Expected %v, but got %v", lastCard, d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("decktesting.txt")
+
+	deckSize := 16
+
+	d := newDeck()
+	d.saveToFile("decktesting.txt")
+
+	loadedDeck := newDeckFromFile("decktesting.txt")
+
+	if len(loadedDeck) != deckSize {
+		t.Errorf("Expected deck length of %v, but got %d", deckSize, len(loadedDeck))
+	}
+
+	os.Remove("decktesting.txt")
 }
