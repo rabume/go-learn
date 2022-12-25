cec477d5bdee9e20804128011fe9660cdb60660e
rabume <info@rabu.me>
2022-12-25 21:35:10 +0100

    Lection: Create map and add, delete values

--- app/main.go
+++ app/main.go
@@ -5,5 +5,23 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi there!")
+	// Create a map with the default "zero" values
+	// Prints out: map[]
+	// var colors1 map[string]string
+	colors1 := make(map[int]string)
+
+	// Add values later on to the above created maps
+	colors1[1] = "#ffffff"
+	colors1[2] = "#ff0000"
+
+	delete(colors1, 2)
+
+	// Create map and add directly some values
+	colors2 := map[string]string{
+		"red":    "#ff0000",
+		"yellow": "ffdd00",
+	}
+
+	fmt.Println(colors1)
+	fmt.Println(colors2)
 }
