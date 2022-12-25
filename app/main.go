b7f78a81e42a5f12dd1df43994984997ee63bc49
rabume <info@rabu.me>
2022-12-25 17:26:03 +0100

    Finished task

--- app/main.go
+++ app/main.go
@@ -1,9 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type number []int32
 
 func main() {
-	fmt.Println("Hi there!")
+
+	numbers := number{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, n := range numbers {
+		checkNumber(n)
+	}
+}
+
+func checkNumber(n int32) {
+	if n%2 == 0 {
+		fmt.Println(n, "is even")
+	} else {
+		fmt.Println(n, "is odd")
+	}
 }
