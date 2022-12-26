488fba73a44dcfba90d8c8b57121f2a1e66f1a88
rabume <info@rabu.me>
2022-12-26 09:32:56 +0100

    Worked with the io package

--- app/main.go
+++ app/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
+type logWriter struct{}
+
 func main() {
-	fmt.Println("Hi there!")
+	resp, err := http.Get("https://google.ch/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+
+	/* body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	} *
+
+	fmt.Printf("%s", body)
+
+	*/
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
