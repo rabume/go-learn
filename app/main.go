3d3999d5af30fdf1d4e313b2c5d5e707dda622ac
rabume <info@rabu.me>
2022-12-26 10:52:57 +0100

    Finished assignment

--- app/main.go
+++ app/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hi there!")
+	// fileToRead := os.Args[1]
+	fileToRead := "myFile.txt"
+
+	content, err := os.ReadFile(fileToRead)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(content))
 }
