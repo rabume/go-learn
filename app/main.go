3ba49026292547a36f0ae5d7cf8719ceb0c0aea5
rabume <info@rabu.me>
2022-12-26 11:39:32 +0100

    First implementation of go routines and channels

--- app/main.go
+++ app/main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hi there!")
+	links := []string{
+		"https://golang.org",
+		"https://rabu.me",
+		"https://amazon.com",
+		"https://google.ch",
+	}
+
+	ch := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, ch)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-ch)
+	}
+}
+
+func checkLink(link string, ch chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		ch <- link + " is down."
+		return
+	}
+
+	ch <- link + " is up and running."
 }
